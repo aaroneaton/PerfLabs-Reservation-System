3dc011a246bb363e3ca48ab6c3df63fdd9439392
Aaron Eaton <aaron@channeleaton.com>
2012-02-15 09:54:20 -0600

    Added user role to equipment table, note to request table, added default user roles

--- mysql_schema.sql
+++ mysql_schema.sql
@@ -2,7 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `reservation_system` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `reservation_system` DEFAULT CHARACTER SET utf8 ;
 USE `reservation_system` ;
 
 -- -----------------------------------------------------
@@ -19,7 +19,7 @@ ENGINE = InnoDB;
 -- Table `reservation_system`.`user_role`
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `reservation_system`.`user_role` (
-  `user_role_id` INT NOT NULL AUTO_INCREMENT ,
+  `user_role_id` INT NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`user_role_id`) )
 ENGINE = InnoDB;
@@ -57,17 +57,19 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`equipment` (
   `name` VARCHAR(128) NOT NULL ,
   `desc` TEXT NOT NULL ,
   `qty` SMALLINT NOT NULL ,
-  `manual` TEXT NULL ,
-  `image` TEXT NULL ,
+  `manual` TEXT NULL DEFAULT NULL ,
+  `image` TEXT NULL DEFAULT NULL ,
   `acq_date` DATE NOT NULL ,
   `equipment_location_id` INT NOT NULL ,
   `status` SMALLINT NOT NULL ,
   `user_id` INT NOT NULL ,
   `equipment_type_id` INT NOT NULL ,
+  `user_role_id` INT NOT NULL ,
   PRIMARY KEY (`equipment_id`) ,
   INDEX `fk_equipment_users1` (`user_id` ASC) ,
   INDEX `fk_equipment_equipment_locations1` (`equipment_location_id` ASC) ,
   INDEX `fk_equipment_equipment_types1` (`equipment_type_id` ASC) ,
+  INDEX `fk_equipment_user_role1` (`user_role_id` ASC) ,
   CONSTRAINT `fk_equipment_users1`
     FOREIGN KEY (`user_id` )
     REFERENCES `reservation_system`.`user` (`user_id` )
@@ -81,6 +83,11 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`equipment` (
   CONSTRAINT `fk_equipment_equipment_types1`
     FOREIGN KEY (`equipment_type_id` )
     REFERENCES `reservation_system`.`equipment_type` (`equipment_type_id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_equipment_user_role1`
+    FOREIGN KEY (`user_role_id` )
+    REFERENCES `reservation_system`.`user_role` (`user_role_id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -120,8 +127,9 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`request` (
   `time_start` TIME NOT NULL ,
   `time_end` TIME NOT NULL ,
   `room_id` INT NOT NULL ,
-  `purpose` TEXT NOT NULL ,
-  `course` VARCHAR(45) NULL ,
+  `purpose` TEXT NULL ,
+  `note` TEXT NULL ,
+  `course` VARCHAR(45) NULL DEFAULT NULL ,
   `status` VARCHAR(45) NOT NULL ,
   PRIMARY KEY (`request_id`) ,
   INDEX `fk_requests_users` (`user_id` ASC) ,
@@ -154,7 +162,7 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`equipment_history` (
   `action` VARCHAR(45) NOT NULL ,
   `equipment_id` INT NOT NULL ,
   `user_id` INT NOT NULL ,
-  `request_id` INT NULL ,
+  `request_id` INT NULL DEFAULT NULL ,
   PRIMARY KEY (`equipment_history_id`) ,
   INDEX `fk_equipment_history_users1` (`user_id` ASC) ,
   INDEX `fk_equipment_history_equipment1` (`equipment_id` ASC) ,
@@ -182,10 +190,10 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `reservation_system`.`room_history` (
   `room_history_id` INT NOT NULL AUTO_INCREMENT ,
-  `datetime` DATETIME NULL ,
+  `datetime` DATETIME NULL DEFAULT NULL ,
   `room_id` INT NOT NULL ,
   `user_id` INT NOT NULL ,
-  `request_id` INT NULL ,
+  `request_id` INT NULL DEFAULT NULL ,
   PRIMARY KEY (`room_history_id`) ,
   INDEX `fk_room_history_users1` (`user_id` ASC) ,
   INDEX `fk_room_history_rooms1` (`room_id` ASC) ,
@@ -225,8 +233,8 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `reservation_system`.`request_comment` (
   `request_comment_id` INT NOT NULL AUTO_INCREMENT ,
-  `datetime` DATETIME NULL ,
-  `comment` TEXT NULL ,
+  `datetime` DATETIME NULL DEFAULT NULL ,
+  `comment` TEXT NULL DEFAULT NULL ,
   `user_id` INT NOT NULL ,
   `request_id` INT NOT NULL ,
   PRIMARY KEY (`request_comment_id`) ,
@@ -294,11 +302,11 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE  TABLE IF NOT EXISTS `reservation_system`.`user_meta` (
   `user_id` INT NOT NULL AUTO_INCREMENT ,
-  `first_name` VARCHAR(45) NULL ,
-  `last_name` VARCHAR(45) NULL ,
-  `email` VARCHAR(128) NULL ,
-  `phone` VARCHAR(12) NULL ,
-  `uin` INT NULL ,
+  `first_name` VARCHAR(45) NULL DEFAULT NULL ,
+  `last_name` VARCHAR(45) NULL DEFAULT NULL ,
+  `email` VARCHAR(128) NULL DEFAULT NULL ,
+  `phone` VARCHAR(12) NULL DEFAULT NULL ,
+  `uin` INT NULL DEFAULT NULL ,
   `user_role_id` INT NOT NULL ,
   PRIMARY KEY (`user_id`) ,
   INDEX `fk_user_meta_users1` (`user_id` ASC) ,
@@ -306,7 +314,7 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`user_meta` (
   CONSTRAINT `fk_user_meta_users1`
     FOREIGN KEY (`user_id` )
     REFERENCES `reservation_system`.`user` (`user_id` )
-    ON DELETE NO ACTION
+    ON DELETE CASCADE
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_meta_user_roles1`
     FOREIGN KEY (`user_role_id` )
@@ -323,7 +331,7 @@ CREATE  TABLE IF NOT EXISTS `reservation_system`.`ci_session_44928` (
   `session_id` VARCHAR(40) NOT NULL DEFAULT 0 ,
   `ip_address` VARCHAR(16) NOT NULL DEFAULT 0 ,
   `user_agent` VARCHAR(120) NOT NULL ,
-  `last_activity` INT(10) UNSIGNED NOT NULL DEFAULT 0 ,
+  `last_activity` INT(10) NOT NULL DEFAULT 0 ,
   `user_data` TEXT NOT NULL ,
   PRIMARY KEY (`session_id`) )
 ENGINE = InnoDB;
@@ -333,3 +341,16 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+-- -----------------------------------------------------
+-- Data for table `reservation_system`.`user_role`
+-- -----------------------------------------------------
+SET AUTOCOMMIT=0;
+USE `reservation_system`;
+INSERT INTO `reservation_system`.`user_role` (`user_role_id`, `name`) VALUES (10, 'Student');
+INSERT INTO `reservation_system`.`user_role` (`user_role_id`, `name`) VALUES (20, 'Studio User');
+INSERT INTO `reservation_system`.`user_role` (`user_role_id`, `name`) VALUES (30, 'Faculty/Staff');
+INSERT INTO `reservation_system`.`user_role` (`user_role_id`, `name`) VALUES (40, 'Manager');
+INSERT INTO `reservation_system`.`user_role` (`user_role_id`, `name`) VALUES (50, 'Administrator');
+
+COMMIT;
