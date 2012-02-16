80e704ab15b54a1736e83286081bd36f2d199e3a
Aaron Eaton <aaron@channeleaton.com>
2012-02-16 11:56:47 -0600

    Added logic comments and seen method

--- application/controllers/equipment.php
+++ application/controllers/equipment.php
@@ -29,23 +29,12 @@ class Equipment extends CI_Controller {
    */
   public function index() {
   
-    $this->load->view( 'templates/header');
-    $this->load->view( 'error/empty_method');
-    $this->load->view( 'templates/footer');
-    $this->output->enable_profiler(TRUE);
-  
-  
-  }
+    // Check if user is administrator OR manager
+    // If not, show 'no access' page
 
-  /**
-   * Method: create();
-   * @access Administrator, Manager
-   *
-   * Creates form to add new equipment
-   *
-   */
-  public function create() {
-  
+    // Else, query database for all equipment
+    // Table should show Custom ID, Name, Qty, Status, User ID
+
     $this->load->view( 'templates/header');
     $this->load->view( 'error/empty_method');
     $this->load->view( 'templates/footer');
@@ -63,6 +52,11 @@ class Equipment extends CI_Controller {
    */
   public function view() {
   
+    // Check if user is an admin OR manager
+    // If not, show 'no access' page
+    //
+    // Else, query database for equipment ID
+
     $this->load->view( 'templates/header');
     $this->load->view( 'error/empty_method');
     $this->load->view( 'templates/footer');
@@ -70,6 +64,37 @@ class Equipment extends CI_Controller {
   
   
   }
+
+  /**
+   * Method: create();
+   * @access Administrator, Manager
+   *
+   * Creates form to add new equipment
+   *
+   */
+  public function create() {
+  
+    // Check if user is an admin OR manager
+    // If not, show 'no access' page
+    //
+    // Else, move on to form
+    //
+    // // Check if form validation has run
+    // // If not, load the form view
+    // //
+    // // Else, create the records in database
+    // // // If record creation passes, redirect to equipment/index and set flash as successful
+    // // //
+    // // // Else, redirect to equipment/create and set flash to fail with error message
+
+    $this->load->view( 'templates/header');
+    $this->load->view( 'error/empty_method');
+    $this->load->view( 'templates/footer');
+    $this->output->enable_profiler(TRUE);
+  
+  
+  }
+
 
   /**
    * Method: edit();
@@ -80,11 +105,50 @@ class Equipment extends CI_Controller {
    */
   public function edit() {
   
+    // Check if user is an admin OR manager
+    // If not, show no access page
+    //
+    // Else, query database for the equipment ID
+    //
+    // // Check if form validation has run
+    // // If not, load the form view
+    // //
+    // // Else, update the record in database
+    // // // If record update passes, redirect to equipment/show and set flash as successful
+    // // //
+    // // // Else, redirect to equipment/edit and set flash to fail with error message
+
     $this->load->view( 'templates/header');
     $this->load->view( 'error/empty_method');
     $this->load->view( 'templates/footer');
     $this->output->enable_profiler(TRUE);
   
+  
+  }
+
+  /**
+   * Method: seen();
+   * @access Administrator, Manager
+   *
+   * Creates equipment history entry to update last seen date
+   *
+   */
+  public function seen() {
+  
+    // Check if user is an admin OR manager
+    // If not, show 'no access' page
+    //
+    // Else, create equipment_history record
+    // // datetime = now();
+    // // action = Seen
+    // // equipment_id = $equipment_id
+    // // user_id = user ID
+    // Redirect to equipment/index with flashdata set to Seen
+
+    $this->load->view( 'templates/header');
+    $this->load->view( 'error/empty_method');
+    $this->load->view( 'templates/footer');
+    $this->output->enable_profiler(TRUE);
   
   }
 
@@ -97,6 +161,11 @@ class Equipment extends CI_Controller {
    */
   public function remove() {
   
+    // Check if user is an admin
+    // If not, show 'no access' page
+    //
+    // Else, remove equipment record from database
+
     $this->load->view( 'templates/header');
     $this->load->view( 'error/empty_method');
     $this->load->view( 'templates/footer');
