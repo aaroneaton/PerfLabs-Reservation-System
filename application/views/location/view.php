711e037cfcdf24310ddfeb38198d6e23978878a3
Aaron Eaton <aaron@channeleaton.com>
2012-02-28 16:24:50 -0600

    Forgot a file

--- application/views/location/view.php
+++ application/views/location/view.php
@@ -1,4 +1,12 @@
 <div class="row">
+  <div class="span8">
+    <h1>Location</h1>
+  </div>
+  <div class="span4">
+    <?php echo anchor( 'location/edit/' . $location['equipment_location_id'], 'Edit', array( 'class' => 'btn btn-primary' ) ); ?>
+  </div>
+</div>
+<div class="row">
 	<div class="span12">
 		<p>
       <b>Building: </b><?php echo $location['bldg']; ?>
